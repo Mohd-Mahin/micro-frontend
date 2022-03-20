b52991875951ac7f355cb77227ce152f6510a775
Mohd-Mahin <mahinit3016@gmail.com>
2022-03-21 02:04:34 +0530

    minor change

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/marketing-app';
 export default () => {
     return (
         <div>
-            <h1>Rendering conainer app</h1>
+            <h1>Rendering conainer app.</h1>
             <MarketingApp />
         </div>
     )
