0f34616a100e35609972139c5725656c16d53689
Mohd-Mahin <mahinit3016@gmail.com>
2022-04-20 15:20:02 +0530

    minor change

--- packages/marketing/src/app.js
+++ packages/marketing/src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
@@ -9,17 +9,18 @@ const generateClassName = createGenerateClassName({
     productionPrefix: 'ma',
 })
 
-export default function App() {
+export default function App(props) {
+    const { history } = props;
 
     return (
         <div>
             <StylesProvider generateClassName={generateClassName}>
-                <BrowserRouter>
+                <Router history={history}>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing} />
                         <Route path="/" component={Landing} />
                     </Switch>
-                </BrowserRouter>
+                </Router>
             </StylesProvider>
         </div>
     )
